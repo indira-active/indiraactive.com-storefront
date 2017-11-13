ab5760ee4e2d16dd11620cfdbe9279e6cc5f41a4
Nathan Guenther <nathanpguenther@gmail.com>
2017-11-12 20:49:35 -0800

    Add Tust badge to checkout

--- additional/google-analytics-scripts.js
+++ additional/google-analytics-scripts.js
@@ -3,3 +3,5 @@ ga('require', 'linkid');
 ga('require', 'linker');
 ga('linker:autoLink', ['indiraactive.com', 'blog.indiraactive.com', 'returns.indiraactive.com', 'track.indiraactive.com']);
 if(__st["cid"]) ga('set', '&uid', __st["cid"]);
+
+;(function(i,s,o,g,r,a,m){i['VNCVIObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=true;a.type='text/javascript';a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://cdn.shopify.com/s/files/1/1918/3859/t/22/assets/vntsc.js?18382667012694740488','vntsci');
