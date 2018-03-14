4f4ac01be0665a39cc8fa2da87d1b95f801b75e0
Nathan Guenther <nathanpguenther@gmail.com>
2018-03-13 20:48:39 -0700

    Update returnly scripts
    - Update to latest smooch SDK before depreciation
    - Remove broken HTML snippet in JavaScript code

--- additional/returnly-scripts.js
+++ additional/returnly-scripts.js
@@ -18,6 +18,18 @@ ga('linker:autoLink', ['indiraactive.com', 'blog.indiraactive.com', 'returns.ind
 (function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"5708096"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");
 // End Bing Ads Universal tag 
 
+// Smooch Head
+!function(e,n,t,r){
+        function o(){try{var e;if((e="string"==typeof this.response?JSON.parse(this.response):this.response).url){var t=n.getElementsByTagName("script")[0],r=n.createElement("script");r.async=!0,r.src=e.url,t.parentNode.insertBefore(r,t)}}catch(e){}}var s,p,a,i=[],c=[];e[t]={init:function(){s=arguments;var e={then:function(n){return c.push({type:"t",next:n}),e},catch:function(n){return c.push({type:"c",next:n}),e}};return e},on:function(){i.push(arguments)},render:function(){p=arguments},destroy:function(){a=arguments}},e.__onWebMessengerHostReady__=function(n){if(delete e.__onWebMessengerHostReady__,e[t]=n,s)for(var r=n.init.apply(n,s),o=0;o<c.length;o++){var u=c[o];r="t"===u.type?r.then(u.next):r.catch(u.next)}p&&n.render.apply(n,p),a&&n.destroy.apply(n,a);for(o=0;o<i.length;o++)n.on.apply(n,i[o])};var u=new XMLHttpRequest;u.addEventListener("load",o),u.open("GET","https://"+r+".webloader.smooch.io/",!0),u.responseType="json",u.send()
+}(window,document,"Smooch","5892a9a16c77636e014b54f3");
+// End Smooch Head
+
+// BEGIN GCR Language Code
+ window.___gcfg = {
+  lang: 'en_US'
+ };
+// END GCR Language Code
+
 
 // Hotjar Tracking Code for https://www.indiraactive.com/ 
 (function(h,o,t,j,a,r){
@@ -31,30 +43,16 @@ ga('linker:autoLink', ['indiraactive.com', 'blog.indiraactive.com', 'returns.ind
 // END Hotjar Tracking Code for https://www.indiraactive.com/ 
 
 
-// Smooch web messenger
-function loadScript(src, callback) { var s, r, t; r = false; s = document.createElement('script'); s.type = 'text/javascript'; s.src = src; s.onload = s.onreadystatechange = function() { if ( !r && (!this.readyState || this.readyState == 'complete') ) { r = true; callback(); } }; t = document.getElementsByTagName('script')[0]; t.parentNode.insertBefore(s, t); } loadScript('https://cdn.smooch.io/smooch.min.js', function() {
-
-    // Store users language as property
-    Smooch.on('ready', function() {
-        var language = window.navigator.userLanguage || window.navigator.language;
-        Smooch.updateUser({
-          properties: {
-          'Language': language
-          }
-        });   
-    }); 
-
-    Smooch.init({
-        appToken: "56jalt3k4p3rpyphnwazq8dzj",
-        serviceUrl: "https://app.smooch.io",
+// Smooch body
+var skPromise = Smooch.init({
+        appId: "5892a9a16c77636e014b54f3",
         customText: {
-          introductionText: 'We try to be quick, so ask us anything!',
-            introAppText: 'Chat with us below or from your favorite app, email, or SMS.',
-            inputPlaceholder: 'Type a message....'                       
+                introductionText: 'We try to be quick, so ask us anything!',
+                introAppText: 'Chat with us below or from your favorite app, email, or SMS.',
+                inputPlaceholder: 'Type a message....'                       
         }
-    });
-  });
-// END Smooch web messenger
+});
+// END Smooch body
 
 
 // Twitter universal website tag code
