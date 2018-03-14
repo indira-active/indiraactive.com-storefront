718449250daafa22da2bac210dfd20e51a2d665b
Nathan Guenther <nathanpguenther@gmail.com>
2018-03-13 22:19:06 -0700

    Add back analytics.js for now

--- additional/returnly-scripts.js
+++ additional/returnly-scripts.js
@@ -1,3 +1,15 @@
+// Universal Analyics (analyics.js) - Google Analytics 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-84828000-1', 'auto');
+ga('send', 'pageview');
+ga('require', 'displayfeatures');
+ga('require', 'linkid');
+ga('require', 'linker');
+ga('linker:autoLink', ['indiraactive.com', 'www.indiraactive.com', 'blog.indiraactive.com', 'returns.indiraactive.com', 'track.indiraactive.com']);
+
 // Bing Ads Universal tag
 (function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"5708096"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");
 // End Bing Ads Universal tag 
